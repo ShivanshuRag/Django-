4fe25c6a9b8a17e72cd0906cca3768f54bb03a59
ShivanshuRag <ragsingh773@gmail.com>
2025-09-09 10:07:45 +0530

    add new things

--- firstDjango/views.py
+++ firstDjango/views.py
@@ -5,11 +5,13 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse("this about me , I a shivanshu ") 
 
-    return render( request , 'index.html')
+    return render( request , 'website/index.html')
 
 
 def about(request):
-    return HttpResponse("this about me , I a shivanshu ")
+    # return HttpResponse("this about me , I a shivanshu ")
+    return render(request , 'website/about.html' )
 
 def contact(request):
-    return HttpResponse("This is my contact : shivanshu@gmail.com ")
+    # return HttpResponse("This is my contact : shivanshu@gmail.com ")
+    return render(request , 'website/contact.html')
