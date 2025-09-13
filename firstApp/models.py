93d6db0e8986e9a9f20cd8ddb7293301c966d870
ShivanshuRag <ragsingh773@gmail.com>
2025-09-13 11:43:07 +0530

    configure relationships between them in model file

--- firstApp/models.py
+++ firstApp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class CourseVariety(models.Model):
@@ -23,6 +24,40 @@ class CourseVariety(models.Model):
     def __str__(self):
         return self.name
     
+
+#  One to many relations
+
+class courseReview(models.Model):
+    course = models.ForeignKey(CourseVariety , on_delete=models.CASCADE , related_name="review")
+    user = models.ForeignKey(User, on_delete=models.CASCADE  )
+    rating = models.IntegerField()
+    comment = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
     
+    def __str__(self):
+        return f'{self.user.username} review for {self.course.name}'
     
+# many to many 
+
+class college(models.Model):
+    college_name = models.CharField(max_length=100)
+    collage_location = models.CharField(max_length=100)
+    course_varities = models.ManyToManyField(CourseVariety , related_name="college")
+
+    def __str__(self):
+        return self.college_name
+    
+
+# one to one 
+
+class Coursecertificate(models.Model):
+    course = models.OneToOneField(CourseVariety , on_delete=models.CASCADE ,related_name='certificate')
+    certificate_number = models.CharField(max_length=100)
+    issued_date = models.DateTimeField(default=timezone.now)
+    valid_until = models.DateTimeField()
+
+
+    def __str__(self):
+        return f'Certificate for {self.course.name}'
+
 
