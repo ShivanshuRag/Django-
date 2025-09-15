6c695af6e31f0d1952ecc8bf22347d0179189085
ShivanshuRag <ragsingh773@gmail.com>
2025-09-15 13:16:08 +0530

    create function of course_college_view

--- firstApp/views.py
+++ firstApp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
-from .models import CourseVariety
-
+from .models import CourseVariety , College
+from .forms import CourseVarietyForm
 # Create your views here.
 
 def first_App(request):
@@ -14,3 +14,18 @@ def course_Detail(request, course_id):
 def course_Price(request , price_id):
     paisa = get_object_or_404(CourseVariety , pk=price_id)
     return render(request , 'firstApp/coursePrice.html' , {'paisa': paisa} )
+
+def course_college_view(request):
+    colleges = None
+    if request.method == 'POST':
+        form = CourseVarietyForm(request.POST)
+        if form.is_valid():
+         course_variety = form.cleaned_data['course_variety']
+         colleges = College.objects.filter(course_varities=course_variety) 
+    
+    else:
+        form = CourseVarietyForm()
+    
+    return render(request , 'firstApp/courseColleges.html',{'form': form ,'colleges':colleges , })
+
+
